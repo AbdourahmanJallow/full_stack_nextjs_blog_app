af1c9fdd707146e80f2b346c4a9b4063351359ad
abdourahman <aj22018057@utg.edu.gm>
2023-07-11 14:41:35 +0000

    toggle drop-down menu

--- app/page.js
+++ app/page.js
@@ -4,10 +4,12 @@ import Timeline from '@/components/Timeline';
 export default function Home() {
     return (
         <section className='flex w-full min-h-screen flex-col items-center justify-start p-24'>
-            <h1 className='text-5xl text-center font-semibold'>
-                <span className='text-teal-300 mb-7'> Express Yourself!</span>{' '}
-                <br /> Unleash Your Thoughts and Ideas <br /> with Our Blogging
-                App!
+            <h1 className='sm:text-5xl text-2xl text-center font-semibold text-[#555]'>
+                <span className='text-teal-300'>
+                    {' '}
+                    Express Yourself! <br className='max-md:hidden' />
+                </span>{' '}
+                Unleash Your Thoughts and Ideas <br /> with Our Blogging App!
             </h1>
 
             <Timeline />
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,13 +4,17 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { useState, useEffect } from 'react';
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
+import { BsBoxArrowRight } from 'react-icons/bs';
+import { BsBoxArrowInLeft } from 'react-icons/bs';
+import { MdPostAdd } from 'react-icons/md';
 
 function Navbar() {
-    const isLoggedIn = false;
+    const isLoggedIn = true;
     const { data: session } = useSession();
+    const [openDropDown, setOpenDropDown] = useState(false);
 
     return (
-        <nav className='flex justify-between items-center p-6 py-3 w-full mb-16 border-b-2 border-solid border-teal-100'>
+        <nav className='flex justify-between items-center p-6 py-1 w-full mb-16 border-b-2 border-solid border-teal-100'>
             <Link href='/'>
                 <Image
                     src='/assets/blog.png'
@@ -30,13 +34,21 @@ function Navbar() {
                             className='primary_btn font-semibold'
                         >
                             New Blog
+                            <MdPostAdd
+                                className='ml-2 inline-block'
+                                size={25}
+                            />
                         </Link>
                         <button
                             type='button'
-                            onCLick={signOut}
+                            onClick={signOut}
                             className='text-xl outline_btn font-bold'
                         >
-                            Sign out
+                            Sign out{' '}
+                            <BsBoxArrowRight
+                                className='ml-2 inline-block'
+                                size={25}
+                            />
                         </button>
                         <Link href='/profile'>
                             <Image
@@ -56,6 +68,10 @@ function Navbar() {
                                 className='outline_btn font-semibold'
                             >
                                 Sign in
+                                <BsBoxArrowInLeft
+                                    className='ml-2 inline-block'
+                                    size={25}
+                                />
                             </button>
                         ))}
                     </>
@@ -63,6 +79,71 @@ function Navbar() {
             </div>
 
             {/* Mobile Navigation */}
+            <div className='flex sm:hidden relative'>
+                {isLoggedIn ? (
+                    <div className='flex flex-col'>
+                        <Image
+                            src='/assets/profile.png'
+                            alt='Profile'
+                            width={30}
+                            height={30}
+                            className='object-contain rounded-full'
+                            onClick={() => setOpenDropDown((prev) => !prev)}
+                        />
+                        {openDropDown && (
+                            <div className='absolute top-full right-0 flex flex-col justify-end items-end bg-[#fff] rounded-xl gap-3 mt-6 p-2 min-w-[250px] border-2 border-solid border-[#f4f4f4]'>
+                                <Link
+                                    href='/profile'
+                                    onClick={() => setOpenDropDown(false)}
+                                    className='text-sm text-teal-300'
+                                >
+                                    My Profile
+                                </Link>
+                                <Link
+                                    href='/new-blog'
+                                    className='text-sm text-teal-300 flex justify-center'
+                                    onClick={() => setOpenDropDown(false)}
+                                >
+                                    New Blog
+                                    <MdPostAdd
+                                        className='ml-2 inline-block'
+                                        size={25}
+                                    />
+                                </Link>
+                                <button
+                                    type='button'
+                                    onClick={() => {
+                                        setOpenDropDown(false);
+                                        signOut;
+                                    }}
+                                    className='text-sm text-teal-300 flex justify-center'
+                                >
+                                    Sign out
+                                    <BsBoxArrowRight
+                                        className='ml-2 inline-block'
+                                        size={25}
+                                    />
+                                </button>
+                            </div>
+                        )}
+                    </div>
+                ) : (
+                    <>
+                        {Object.values([1]).map((item) => (
+                            <button
+                                type='button'
+                                className='outline_btn font-semibold'
+                            >
+                                Sign in
+                                <BsBoxArrowInLeft
+                                    className='ml-2 inline-block'
+                                    size={25}
+                                />
+                            </button>
+                        ))}
+                    </>
+                )}
+            </div>
         </nav>
     );
 }
